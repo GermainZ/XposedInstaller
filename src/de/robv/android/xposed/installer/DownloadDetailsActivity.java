6deb4fb2ed9411b193bd5a64c5bae7b9c531f42e
GermainZ <germanosz@gmail.com>
2014-03-02 19:09:10 +0200

    Add ViewPager for download details: first tab shows descriptions, second tab versions

--- src/de/robv/android/xposed/installer/DownloadDetailsActivity.java
+++ src/de/robv/android/xposed/installer/DownloadDetailsActivity.java
@@ -1,20 +1,49 @@
 package de.robv.android.xposed.installer;
 
-import android.app.FragmentTransaction;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v13.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.MenuItem;
 
 public class DownloadDetailsActivity extends XposedDropdownNavActivity {
+
+	private ViewPager mPager;
+	private PagerAdapter mPagerAdapter;
+	private String[] mPages;
+	private String mPackageName;
+
+	private static final int DOWNLOAD_DESCRIPTION = 0;
+	private static final int DOWNLOAD_VERSIONS = 1;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		String packageName = getIntent().getData().getSchemeSpecificPart();
-		DownloadDetailsFragment detailsFragment = DownloadDetailsFragment.newInstance(packageName);
+		setContentView(R.layout.activity_download_details);
 
-		FragmentTransaction tx = getFragmentManager().beginTransaction();
-		tx.replace(android.R.id.content, detailsFragment);
-		tx.commit();
+		mPackageName = getIntent().getData().getSchemeSpecificPart();
+
+		mPages = new String[]{getString(R.string.description_page), getString(R.string.versions_page)};
+		mPager = (ViewPager) findViewById(R.id.download_pager);
+		mPagerAdapter = new ScreenSlidePagerAdapter(getFragmentManager());
+		mPager.setAdapter(mPagerAdapter);
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				onBackPressed();
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
@@ -28,4 +57,33 @@ public class DownloadDetailsActivity extends XposedDropdownNavActivity {
 		intent.putExtra(XposedInstallerActivity.EXTRA_OPEN_TAB, TAB_DOWNLOAD);
 		return intent;
 	}
+
+	private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+		public ScreenSlidePagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			switch (position) {
+				case DOWNLOAD_DESCRIPTION:
+					return DownloadDetailsFragment.newInstance(mPackageName);
+				case DOWNLOAD_VERSIONS:
+					return DownloadDetailsVersionsFragment.newInstance(mPackageName);
+				default:
+					return null;
+			}
+		}
+
+		@Override
+		public int getCount() {
+			return mPages.length;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return mPages[position];
+		}
+	}
+
 }
