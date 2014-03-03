0403aca9c9b72906e23e88713bf2e98e51e13351
GermainZ <germanosz@gmail.com>
2014-03-03 18:16:02 +0200

    Add ExpandableTextView which acts like the Play Store's "What's New"/"Description" expandable text panels

---
+++ src/de/robv/android/xposed/installer/widget/ExpandableTextView.java
@@ -0,0 +1,75 @@
+package de.robv.android.xposed.installer.widget;
+
+import android.content.Context;
+import android.text.method.MovementMethod;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import de.robv.android.xposed.installer.R;
+
+public class ExpandableTextView extends LinearLayout {
+
+	private final TextView textView;
+	private final ImageView imageView;
+
+	private final static int MAX_LINES = 5;
+
+	public ExpandableTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setOrientation(VERTICAL);
+
+		textView = new TextView(context);
+		imageView = new ImageView(context);
+
+		textView.setMaxLines(MAX_LINES);
+
+		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT);
+		layoutParams.gravity = Gravity.RIGHT;
+		imageView.setLayoutParams(layoutParams);
+		imageView.setImageResource(R.drawable.ic_expand);
+
+		addView(textView);
+		addView(imageView);
+	}
+
+	public void setText(CharSequence text) {
+		textView.setText(text);
+	}
+
+	public void setMovementMethod(MovementMethod method) {
+		textView.setMovementMethod(method);
+	}
+
+	/** The ImageView would normally handle the click, we set the OnClickListener to both the ImageView
+	 * and the TextView so that we can click anywhere to expand/collapse */
+	public void setOnClickListener(OnClickListener listener) {
+		textView.setOnClickListener(listener);
+		imageView.setOnClickListener(listener);
+	}
+
+	/** Collapses or expands the view if necessary.
+	 * Should be called from within an OnClickListener and when the view is created or in the custom
+	 * adapter's getView if in a ListView, as the collapsed state must be managed there to avoid it
+	 * getting recycled, */
+	public void collapseView(boolean collapse) {
+		if (textView.getLineCount() <= MAX_LINES) {
+			imageView.setVisibility(GONE);
+			return;
+		} else {
+			imageView.setVisibility(VISIBLE);
+		}
+
+		if (collapse) {
+			textView.setMaxLines(MAX_LINES);
+			imageView.setRotationX(0);
+		} else {
+			textView.setMaxLines(Integer.MAX_VALUE);
+			imageView.setRotationX(180);
+		}
+	}
+
+}
